75a137e41bb8588e8bba161e5c838c291dace2a9
Eirene Oyakhilome <33825632+Peace99@users.noreply.github.com>
2021-09-17 13:07:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const input = document.getElementById("input")
+const grid = document.querySelector(".grid")
+
+window.addEventListener('load', randomImage)
+
+input.addEventListener('keydown', function(event){
+  if(event.key === 'Enter')
+  randomImage() = ''
+  loadImg()
+})
+
+function randomImage(){
+    let client_id = 'M38Av8f5_FeX-4j62y4Zh2uDCW1cU9NAblOyZXeGbiY'
+    let endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`
+
+    let images = document.querySelector(".slide-img")
+
+    fetch(endpoint).then(img =>{
+        return img.json()
+    }).then(imagedata => {
+        images.setAttribute("src", imagedata.urls.regular)
+    })
+
+    
+}
+
+function loadImg(){
+  removeImg()
+  const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=12&client_id=M38Av8f5_FeX-4j62y4Zh2uDCW1cU9NAblOyZXeGbiY'
+
+  fetch(url).then(res =>{
+      if(res.ok)
+      return res.json()
+      else{
+        alert(res.status)
+      }
+  }).then(data => {
+      const imageArray = []
+      for (let i = 0; i < data.results.length; i++) {
+        imageArray [i] = document.createElement('div');
+        imageArray [i].className = 'img'
+        imageArray [i].style.backgroundImage = 'url('+data.results[i].urls.raw+')'
+        imageArray [i].addEventListener('dblclick', function(){
+            window.open(data.results[i].links.download,'_blank')
+          })
+        grid.appendChild(imageArray[i])
+      }
+  })
+}
+
+function removeImg(){
+    grid.innerHTML = ''
+}
